83dffff529de1794cb07a2e8763f6165ae48c6d2
Harel Karni <MountainGodRays@gmail.com>
2022-11-09 00:51:16 +0200

    Added employee logging.

---
+++ EmployeeLogger/EmployeeLogger.cs
@@ -0,0 +1,107 @@
+﻿namespace Logging;
+
+/// <summary>
+/// Logging object for employees.
+/// Each employee has their own logger.
+/// The logger writes to a log file in CSV format.
+/// The log includes timestamps, the actions of the employee, and extra details if needed.
+/// </summary>
+public class EmployeeLogger
+{
+    private string _id = null!;
+
+    public string ID
+    {
+        get => _id;
+        set => _id = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    private readonly string _name = null!;
+
+    public string Name
+    {
+        get => _name;
+        init => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    private readonly string _path = null!;
+
+    public string Path
+    {
+        get => _path;
+        init => _path = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    /// <summary>
+    /// Create an EmployeeLogger object for a specific Employee.
+    /// </summary>
+    /// <param name="employee">The employee to log</param>
+    public EmployeeLogger(Employee employee)
+    {
+        ID = employee.ID;
+        Name = employee.Name;
+        Path = $".\\logs\\{ID}\\{DateTime.Today:yyyy-MM-dd}.log";
+        StartLog();
+    }
+
+    /// <summary>
+    /// Create an EmployeeLogger object for an employee with the entered properties.
+    /// </summary>
+    /// <param name="id">ID of the employee to log</param>
+    /// <param name="name">name of the employee to log</param>
+    public EmployeeLogger(string id, string name)
+    {
+        ID = id;
+        Name = name;
+        Path = $".\\logs\\{ID}\\{DateTime.Today:yyyy-MM-dd}.log";
+        StartLog();
+    }
+
+    /// <summary>
+    /// Log an action made by the employee.
+    /// The action will be logged with the current time.
+    /// </summary>
+    /// <param name="actionType">The type of action made.</param>
+    /// <param name="details">Details about the specific action.</param>
+    public void LogAction(string actionType, string details = "")
+    {
+        var fileStream = new FileStream(Path, FileMode.Append, FileAccess.Write);
+        var streamWriter = new StreamWriter(fileStream);
+        streamWriter.WriteLine($"\"{DateTime.UtcNow}\",\"{actionType}\",\"{details}\"");
+        
+        streamWriter.Close();
+    }
+    
+    private void StartLog()
+    {
+        FileInfo file = new FileInfo(Path);
+        file.Directory.Create(); // If the directory already exists, does nothing
+        var fileStream = new FileStream(Path, FileMode.Append, FileAccess.Write);
+        var streamWriter = new StreamWriter(fileStream);
+        streamWriter.WriteLine($"Timestamp (UTC), Action, Details");
+        streamWriter.Close();
+        
+        LogAction("Log Start", $"Employee: {ID} {Name}");
+    }
+
+    /// <summary>
+    /// Log shift start.
+    /// </summary>
+    public void StartShift()
+    {
+        LogAction("Shift Start");
+    }
+    
+    /// <summary>
+    /// Log shift end.
+    /// </summary>
+    public void EndShift()
+    {
+        LogAction("Shift End");
+    }
+
+    public override string ToString()
+    {
+        return $"Employee Logger for: {Name}, {ID}.";
+    }
+}
---
+++ EmployeeLogger/TestObjects.cs
@@ -0,0 +1,49 @@
+﻿namespace Logging;
+
+public class TestProgram
+{
+    public static void Main()
+    {
+        Employee emp1 = new Employee("H", "1");
+        
+        emp1.StartShift();
+        emp1.DoAction();
+        emp1.DoSomething();
+        emp1.EndShift();
+    }
+}
+
+public class Employee
+{
+    public string Name { get; set; }
+    public string ID { get; set; }
+    private EmployeeLogger logger;
+
+    public Employee(string name, string id)
+    {
+        this.Name = name ?? throw new ArgumentNullException(nameof(name));
+        this.ID = id ?? throw new ArgumentNullException(nameof(id));
+
+        logger = new EmployeeLogger(this);
+    }
+
+    public void StartShift()
+    {
+        logger.StartShift();
+    }
+    
+    public void EndShift()
+    {
+        logger.EndShift();
+    }
+
+    public void DoAction()
+    {
+        logger.LogAction("An action");
+    }
+    
+    public void DoSomething()
+    {
+        logger.LogAction("A thing", "some");
+    }
+}
