86448ac1365ba0e84853c3ce5e2d6e245f8ce355
IgnitedIce <s81.private@gmail.com>
2022-11-09 14:32:13 +0200

    Add: month check for salary report

--- GeneralManagement/SalaryReport.cs
+++ GeneralManagement/SalaryReport.cs
@@ -11,29 +11,37 @@ namespace Salary
 			this.employee = employee;
 		}
 
-		public double GetTotalHours()
+		public double TotalWorkHours(DateTime dateTime)
 		{
 			// Defaults to 0
 			// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/default-values
 			double totalHours;
 
-			for (short i = 0; i < this.employee.GetShifts().Length; i++)
+			// Returns all shift objects in month of datetime
+			Shift[] shifts = this.employee.GetShifts(dateTime);
+
+			// Increments totalHours by the shift time for each shift
+			for (short i = 0; i < shifts.Length; i++)
 			{
-				EmployeeShift shift = this.employee.GetShifts()[i];
+				EmployeeShift shift = shifts[i];
 				totalHours += Convert.ToDouble(shift.shiftTime());
 			}
 
 			return totalHours;
 		}
 
-		public double GetSalary()
+		// Returns monthly salary for this employee
+		public double GetSalary(DateTime dateTime = DateTime.Now)
 		{
-			return this.employee.salaryPerHour * this.employee.GetTotalHours();
+			return this.employee.GetSalaryPerHour() * this.TotalWorkHours(dateTime);
 		}
 
 		public override string ToString()
 		{
-			return string.Format("Salary for {0}: {1}", this.employee.GetFullName(), this.GetSalary());
+			return string.Format("Salary for {0}: {1}",
+				this.employee.GetFullName(),
+				this.GetSalary()
+			);
 		}
 	}
 }
