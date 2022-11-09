68bf10109ee6f05ffcfa34ad815a557fce73e164
Ariel4047 <arielbennatan68@gmail.com>
2022-11-09 14:51:19 +0200

    Added InventoryManager with basic reporting

---
+++ InventoryManager/InventoryManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace InventoryReportTest
+{
+    public class InventoryManager
+    {
+        private LinkedList<Card> _cardsSold = new LinkedList<Card>();
+        private LinkedList<Board> _boardsSold = new LinkedList<Board>();
+
+        private LinkedList<Card> _cardsStored = new LinkedList<Card>();
+        private LinkedList<Board> _boardsStored = new LinkedList<Board>();
+
+        public InventoryManager()
+        {
+        }
+
+        public InventoryManager(string dbPath)
+        {
+            LoadComponents(dbPath);
+        }
+
+        public void SellCard(Card c)
+        {
+            _cardsSold.AddLast(c);
+            _cardsStored.Remove(c);
+        }
+
+        public void SellBoard(Board b)
+        {
+            _boardsSold.AddLast(b);
+            _boardsStored.Remove(b);
+        }
+
+        public void AddCard(Card c)
+        {
+            _cardsStored.AddLast(c);
+        }
+
+        public void AddBoard(Board b)
+        {
+            _boardsStored.AddLast(b);
+        }
+
+
+        private void LoadComponents(string dbPath)
+        {
+            // TODO: complete this
+        }
+
+        public void InventoryReport()
+        {
+            string path = $".\\InventoryReport\\{DateTime.Now: yyyy-MM-dd hhmmss}.log";
+
+            FileInfo file = new FileInfo(path);
+            file.Directory.Create(); // If the directory already exists, does nothing
+            var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+            var streamWriter = new StreamWriter(fileStream);
+            streamWriter.WriteLine($"Inventory report as of {DateTime.Now: yyyy-MM-dd hh:mm:ss}:");
+
+            streamWriter.WriteLine("Stored Cards:");
+            double cardSum = 0;
+            foreach (Card card in _cardsStored)
+            {
+                streamWriter.WriteLine($"card type {card.type}, id: {card.id}, cost: {card.cost}₪");
+                cardSum += card.cost;
+            }
+            streamWriter.WriteLine($"Card sum: {cardSum}₪\n");
+
+            streamWriter.WriteLine("Stored Boards:");
+            double boardSum = 0;
+            foreach (Board board in _boardsStored)
+            {
+                streamWriter.WriteLine($"board type {board.type}, id: {board.id}, cost: {board.cost}₪");
+                boardSum += board.cost;
+            }
+            streamWriter.WriteLine($"Board sum: {boardSum}₪\n");
+
+            streamWriter.WriteLine($"Total sum: {boardSum + cardSum}₪");
+            streamWriter.Close();
+        }
+    }
+}
---
+++ InventoryManager/TestObjects.cs
@@ -0,0 +1,54 @@
+﻿namespace InventoryReportTest
+{
+    static class A
+    {
+        public static void Main(string[] args)
+        {
+            InventoryManager inventoryManager = new InventoryManager();
+
+            inventoryManager.InventoryReport();
+
+            Card[] cards = new Card[6];
+            Board[] boards = new Board[6];
+            for (int i = 1; i <= 6; i++)
+            {
+                cards[i - 1] = new Card($"{i}", $"{i}", ((30 + i) / 2) * i, inventoryManager);
+                boards[i - 1] = new Board($"{i}", $"{8 - i}", ((100 + i) / 2) * i, inventoryManager);
+            }
+
+            inventoryManager.InventoryReport();
+        }
+    }
+    
+    public class Board
+    {
+        public string type;
+        public string id;
+        public double cost;
+        public Board(string id, string type, double cost, InventoryManager IM)
+        {
+            this.id = id;
+            this.type = type;
+            this.cost = cost;
+            
+            IM.AddBoard(this);
+        }
+    }
+    
+    public class Card
+    {
+        public string type;
+        public string id;
+        public double cost;
+
+        public Card(string id, string type, double cost, InventoryManager IM)
+        {
+            this.id = id;
+            this.type = type;
+            this.cost = cost;
+            
+            
+            IM.AddCard(this);
+        }
+    }
+}
