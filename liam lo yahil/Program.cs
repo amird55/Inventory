eadb73ce25a1a7f6081fdae9fff643661b715f72
TheF1NALCntdwn <116719335+TheF1NALCntdwn@users.noreply.github.com>
2022-11-16 12:58:08 +0200

    Update Program.cs

--- liam lo yahil/Program.cs
+++ liam lo yahil/Program.cs
@@ -7,7 +7,19 @@ namespace liam_lo_yahil
     {
         static void Main(string[] args)
         {
-            IDictionary<int, int> pleasework = Part.ziv(2, 6);
+            kits[] kit =3 new kits[4];
+            kit[0] = new kits();
+            kit[1] = new kits();
+            kit[2] = new kits();
+            kit[3] = new kits();
+            cards[] card = new cards[4];
+            card[0] = new cards();
+            card[1] = new cards();
+            card[2] = new cards();
+            card[3] = new cards();
+            
+            IDictionary<int, int> pleasework = Part.ziv(2, 6,kit, card);
+            
             foreach (var item in pleasework)
             {
                 Console.WriteLine("key= {0}, value={1}",item.Key,item.Value);
