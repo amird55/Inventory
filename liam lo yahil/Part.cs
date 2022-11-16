319e6c540d3bf9993676797f65ff75b13b0d22d2
TheF1NALCntdwn <116719335+TheF1NALCntdwn@users.noreply.github.com>
2022-11-16 12:57:07 +0200

    Update Part.cs

--- liam lo yahil/Part.cs
+++ liam lo yahil/Part.cs
@@ -8,18 +8,9 @@ namespace liam_lo_yahil
     class Part
     {
         
-        public static IDictionary<int, int> ziv(int idkit, int amount) //traget[id of card,amount]
+        public static IDictionary<int, int> ziv(int idkit, int amount,kits[] kit, cards[] card) //traget[id of card,amount]
            {
-            kits[] kit =3 new kits[4];
-            kit[0] = new kits();
-            kit[1] = new kits();
-            kit[2] = new kits();
-            kit[3] = new kits();
-            cards[] card = new cards[4];
-            card[0] = new cards();
-            card[1] = new cards();
-            card[2] = new cards();
-            card[3] = new cards();
+            
 
 
             IDictionary<int, int> totalparts = new Dictionary<int, int>();
